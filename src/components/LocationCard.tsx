e232ec79149f74a84493a9be4457c9f9953aa7dc
TamarLaufer <tamarlaufer@gmail.com>
2025-12-06 23:16:51 +0200

    add src folder

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Location } from '../types/locationTypes';
+import { Location } from '../src/types/locationTypes';
 
 type Props = {
   location: Location;
